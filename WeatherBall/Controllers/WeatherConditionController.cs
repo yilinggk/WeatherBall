23e6b72370c0bbd99c165df7ca3420492aa6af81
Yiling Kang <yiling2kang@gmail.com>
2020-11-19 02:28:26 -0800

    Added gitignore

--- WeatherBall/Controllers/WeatherConditionController.cs
+++ WeatherBall/Controllers/WeatherConditionController.cs
@@ -8,6 +8,7 @@ using WeatherBall.Models;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace WeatherBall.Controllers
 {
@@ -15,19 +16,23 @@ namespace WeatherBall.Controllers
     [Route("[controller]")]
     public class WeatherConditionController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
         private readonly ILogger<WeatherConditionController> _logger;
         const string uri = "http://api.openweathermap.org/data/2.5/";
-        const string apiKey = "3e4a7ae796b3f41ae36818449149f244";
 
-        public WeatherConditionController(ILogger<WeatherConditionController> logger)
+        public WeatherConditionController(IConfiguration config, ILogger<WeatherConditionController> logger)
         {
             _logger = logger;
+            _config = config;
         }
 
         [HttpGet]
         public IEnumerable<WeatherCondition> GetWeather()
         {
-            string uriParameters = $"weather?q=vancouver,bc,can&appid=3e4a7ae796b3f41ae36818449149f244&units=metric";
+            string apiKey = _config.GetValue<string>("WeatherKey");
+            Console.WriteLine(apiKey);
+            string uriParameters = $"weather?q=vancouver,bc,can&appid={apiKey}&units=metric";
             var res = APICall.RunAsync<WeatherRecord>(uri, uriParameters).GetAwaiter().GetResult();
 
             var weatherConditions = res.Weather;
---
+++ WeatherBall/Models/WeatherConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherBall.Models
+{
+    public class WeatherConfig
+    {
+        public String WeatherAPIKey { get; set; }
+    }
+}
